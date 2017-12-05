9e172910b7f2ebd2d9adf30e2f77d5764ded0213
Eric Mai <enmai1988@gmail.com>
2017-12-05 12:30:52 -0800

    updated infinite scroll logics

--- src/app/directives/infinite-scroll.directive.ts
+++ src/app/directives/infinite-scroll.directive.ts
@@ -1,20 +1,42 @@
-import { Directive, Input, Inject, HostListener } from '@angular/core';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/exhaustMap';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/do';
+import { Observable } from 'rxjs/Observable';
+import { Directive, ElementRef, Input, Inject, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appInfiniteScroll]'
 })
 export class InfiniteScrollDirective {
+  @Input() scrollPercent: number;
   @Input() scrollCallback: Function;
-  @HostListener('window:scroll')
-  onScroll() {
-    let offset = document.documentElement.scrollTop || window.pageYOffset;
-    let scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+  scrollPosition: number;
+  scrollEvent: Observable<any>;
 
-    if (offset >= scrollHeight) {
-      this.scrollCallback();
-    }
+  constructor(private el: ElementRef) { }
+  
+  ngOnInit() {
+    this.scrollPercent = this.scrollPercent || 1;
   }
 
-  constructor() { }
+  ngAfterViewInit() {
+    this.scrollEvent = Observable.fromEvent(window, 'scroll');
+    this.streamScrollEvent().exhaustMap(() => this.scrollCallback()).subscribe(() => {});
+  }
+
+  streamScrollEvent() {
+    return this.scrollEvent
+      .map((e: any) => ({
+        sT: e.target.documentElement.scrollTop,
+        sH: e.target.documentElement.scrollHeight,
+        cH: e.target.documentElement.clientHeight
+      }))
+      .filter((position) => this.isBeyondScrollPercent(position));
+  }
+
+  isBeyondScrollPercent(position):boolean {
+    return position.sT >= ((position.sH - position.cH) * this.scrollPercent);
+  }
   
 }
